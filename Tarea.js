562a0ab9ed632244f2052ce39ef8942e58305af4
facundo <facurmedina@gmail.com>
2023-11-22 09:39:43 -0300

    Listar Productos

--- Tarea.js
+++ Tarea.js
@@ -2,6 +2,23 @@
 // o una letra
 // ● Eliminar producto del carrito
 
-function filtrar(params) {
+// function filtrar(params) {
     
+// }
+
+
+// ● Listar los productos del carrito
+
+const carritoDeCompras = [
+    { Nombre: "Producto 1", Precio: 1500},
+    { Nombre: "Producto 2", Precio: 2500},
+    { Nombre: "Producto 3", Precio: 3500}
+];
+
+function mostrarProductosDelCarrito() {
+    for (let index = 0; index < carritoDeCompras.length; index++) {
+        console.log(carritoDeCompras[index].Nombre)
+        console.log(carritoDeCompras[index].Precio)    
+    }       
 }
+// console.log(`${Producto.Nombre}` + "Precio: " + `${Producto.Precio}`)
