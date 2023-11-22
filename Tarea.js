42fd9fa163befbcdc56f578e1ae6b36065bcf28c
Enzo Castro <enzocastro110@gmail.com>
2023-11-22 09:37:26 -0300

    Buscador

--- Tarea.js
+++ Tarea.js
@@ -1,7 +1,15 @@
-// ● Filtrar productos que coincidan con una palabra, parte de ella
-// o una letra
-// ● Eliminar producto del carrito
 
-function filtrar(params) {
-    
-}
+const carritoDeCompras = 
+    [
+        { nombre: "Producto 1", Precio: 1500},
+        { nombre: "Producto 2", Precio: 2500},
+        { nombre: "Producto 3", Precio: 3500}
+    ];
+
+function buscarProductos(carritoDeCompras , nombre) {
+
+    for (let index = 0; index < array.length; index++) {
+        if (carritoDeCompras[index].nombre === nombre) {
+            console.log("Producto buscado: " + carritoDeCompras[index].nombre + " y su precio es: " + carritoDeCompras[index].precio)
+    } else { console.log("No se encontró lo que buscas, bro")
+}}}
